629172d119243f20d180e68ee15cd5bf711010e9
shaoyingzhu <yingzhu.shao@newtouch.cn>
2018-05-09 10:48:57 +0800

    初始化项目,基础框架搭建

---
+++ src/main/java/com/mooc/house/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.mooc.house.controller;
+
+import com.mooc.house.model.User;
+import com.mooc.house.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+public class UserController {
+
+  @Autowired
+  private UserService userService;
+
+  @RequestMapping("msg")
+  public List<User> accountsRegister() {
+    return userService.getUsers();
+  }
+
+}
